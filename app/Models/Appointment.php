c4a58d7c5515a179196ebe0a47e631a0243908c0
rashad al shukh <rashad000089@gmail.com>
2025-08-16 15:26:58 +0300

    Add patient_id column to appointments

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -11,8 +11,8 @@ class Appointment extends Model
     //use Translatable;
     use HasFactory;
     //public $translatedAttributes = ['name'];
-    public $fillable= ['name','email','phone','notes','doctor_id','section_id','type','appointment'];
-
+    public $fillable= ['name','email','phone','notes','doctor_id','section_id','patient_id','type','appointment'];
+    
     public function doctor()
     {
         return $this->belongsTo(Doctor::class,'doctor_id');
@@ -21,5 +21,9 @@ class Appointment extends Model
     public function section()
     {
         return $this->belongsTo(Section::class,'section_id');
+    }
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class,'patient_id');
     }
 }
