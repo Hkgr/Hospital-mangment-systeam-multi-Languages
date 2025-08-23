618d5477ed5d21ffb934165eced7f3e06a3fd0e8
rashad al shukh <rashad000089@gmail.com>
2025-08-23 13:16:57 +0300

    Update patient name in invoices view

--- app/Http/Controllers/Dashboard_Doctor/PatientDetailsController.php
+++ app/Http/Controllers/Dashboard_Doctor/PatientDetailsController.php
@@ -6,15 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Models\Diagnostic;
 use App\Models\Laboratorie;
 use App\Models\Ray;
+use App\Models\Patient;
 use Illuminate\Http\Request;
 
 class PatientDetailsController extends Controller
 {
     public function index($id){
-
+        $patient = Patient::findOrFail($id);
         $patient_records = Diagnostic::where('patient_id',$id)->get();
         $patient_rays = Ray::where('patient_id',$id)->get();
         $patient_Laboratories  = Laboratorie::where('patient_id',$id)->get();
-        return view('Dashboard.doctor.invoices.patient_details',compact('patient_records','patient_rays','patient_Laboratories'));
-    }
+        return view('Dashboard.doctor.invoices.patient_details',compact('patient','patient_records','patient_rays','patient_Laboratories'));
+        }
 }
--- resources/views/Dashboard/doctor/invoices/patient_details.blade.php
+++ resources/views/Dashboard/doctor/invoices/patient_details.blade.php
@@ -10,9 +10,9 @@
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">Pages</h4><span
-                    class="text-muted mt-1 tx-13 mr-2 mb-0">/ Empty</span>
-            </div>
+                <h4 class="content-title mb-0 my-auto">القصة السريرة</h4><span
+                class="text-muted mt-1 tx-13 mr-2 mb-0">/ {{ $patient->name }}</span>
+                        </div>
         </div>
     </div>
     <!-- breadcrumb -->
