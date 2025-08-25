223b78c55a00d73819adea0c587a800e83b61855
rashad al shukh <rashad000089@gmail.com>
2025-08-26 00:21:43 +0300

    fixing appointment

--- resources/views/Dashboard/appointments/approval.blade.php
+++ resources/views/Dashboard/appointments/approval.blade.php
@@ -1,6 +1,6 @@
 <!-- Deleted insurance -->
 <div class="modal fade" id="approval{{ $appointment->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
+    aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -15,17 +15,17 @@
                     @csrf
                     <input type="hidden" name="id" value="{{ $appointment->id }}">
                     <p class="mg-b-20">{{$appointment->patient->name}}</p>
-                                        <!--div-->
+                    <!--div-->
                     <div class="col-md-12 col-xl-12 col-xs-12 col-sm-12">
                         <div class="card">
                             <div class="card-body">
-                                    <div class="input-group col-md-12">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text">
-                                                <i class="typcn typcn-calendar-outline tx-24 lh--9 op-6"></i>
-                                            </div>
-                                        </div><input class="form-control" name="appointment" id="datetimepicker" type="text" value="{{date('Y-m-d H:i')}}">
-                                    </div>
+                                <div class="input-group col-md-12">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">
+                                            <i class="typcn typcn-calendar-outline tx-24 lh--9 op-6"></i>
+                                        </div>
+                                    </div><input class="form-control" name="appointment" id="datetimepicker" type="text" value="{{date('Y-m-d H:i')}}">
+                                </div>
 
                             </div>
                         </div>
@@ -34,7 +34,7 @@
 
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary"
-                                data-dismiss="modal">{{ trans('insurance.close') }}</button>
+                            data-dismiss="modal">{{ trans('insurance.close') }}</button>
                         <button class="btn btn-success">{{ trans('insurance.save') }}</button>
                     </div>
                 </form>
