2de91b73b38af112ee0116a8777bbe1fd270fcc2
rashad al shukh <rashad000089@gmail.com>
2025-08-29 02:47:51 +0300

    Wrap doctor names in anchor tags

--- resources/views/Dashboard/dashboard_LaboratorieEmployee/invoices/completed_invoices.blade.php
+++ resources/views/Dashboard/dashboard_LaboratorieEmployee/invoices/completed_invoices.blade.php
@@ -1,34 +1,34 @@
 @extends('Dashboard.layouts.master')
 @section('title')
-    الفواتير المكتملة
+الفواتير المكتملة
 @stop
 @section('css')
 
 
-    <link href="{{URL::asset('dashboard/plugins/notify/css/notifIt.css')}}" rel="stylesheet"/>
+<link href="{{URL::asset('dashboard/plugins/notify/css/notifIt.css')}}" rel="stylesheet" />
 
 @section('page-header')
-    <!-- breadcrumb -->
-    <div class="breadcrumb-header justify-content-between">
-        <div class="my-auto">
-            <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">الفواتير المكتملة</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ الفواتير</span>
-            </div>
+<!-- breadcrumb -->
+<div class="breadcrumb-header justify-content-between">
+    <div class="my-auto">
+        <div class="d-flex">
+            <h4 class="content-title mb-0 my-auto">الفواتير المكتملة</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ الفواتير</span>
         </div>
     </div>
-    <!-- breadcrumb -->
+</div>
+<!-- breadcrumb -->
 @endsection
 @section('content')
-    @include('Dashboard.messages_alert')
-    <!-- row -->
-    <!-- row opened -->
-    <div class="row row-sm">
-        <div class="col-xl-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table text-md-nowrap" id="example1">
-                            <thead>
+@include('Dashboard.messages_alert')
+<!-- row -->
+<!-- row opened -->
+<div class="row row-sm">
+    <div class="col-xl-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table text-md-nowrap" id="example1">
+                        <thead>
                             <tr>
                                 <th>#</th>
                                 <th>تاريخ الفاتورة</th>
@@ -37,45 +37,44 @@
                                 <th>المطلوب</th>
                                 <th>حالة الفاتورة</th>
                             </tr>
-                            </thead>
-                            <tbody>
+                        </thead>
+                        <tbody>
                             @foreach($invoices as $invoice)
-                                <tr>
-                                    <td>{{ $loop->iteration}}</td>
-                                    <td>{{ $invoice->created_at }}</td>
-                                    <td><a href="{{route('laboratorie.patient_details',$invoice->patient_id)}}">{{ $invoice->Patient->name }}</a></td>
-                                    <td>{{ $invoice->doctor->name }}</td>
-                                    <td>{{ $invoice->description }}</td>
-                                    <td>
-                                        @if($invoice->case == 0)
-                                            <span class="badge badge-danger">تحت الاجراء</span>
-                                        @else
-                                            <span class="badge badge-success">مكتملة</span>
-                                        @endif
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td>{{ $loop->iteration}}</td>
+                                <td>{{ $invoice->created_at }}</td>
+                                <td><a href="{{route('laboratorie.patient_details',$invoice->patient_id)}}">{{ $invoice->Patient->name }}</a></td>
+                                <td><a href="{{ route('laboratorie.doctor_images', $invoice->doctor_id) }}">{{ $invoice->doctor->name }}</a></td>
+                                <td>{{ $invoice->description }}</td>
+                                <td>
+                                    @if($invoice->case == 0)
+                                    <span class="badge badge-danger">تحت الاجراء</span>
+                                    @else
+                                    <span class="badge badge-success">مكتملة</span>
+                                    @endif
+                                </td>
+                            </tr>
                             @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div><!-- bd -->
+                        </tbody>
+                    </table>
+                </div>
             </div><!-- bd -->
-        </div>
-        <!--/div-->
-
-        <!-- /row -->
+        </div><!-- bd -->
     </div>
-    <!-- row closed -->
+    <!--/div-->
 
-    <!-- Container closed -->
+    <!-- /row -->
+</div>
+<!-- row closed -->
 
-    <!-- main-content closed -->
+<!-- Container closed -->
+
+<!-- main-content closed -->
 @endsection
 @section('js')
 
-    <!--Internal  Notify js -->
-    <script src="{{URL::asset('dashboard/plugins/notify/js/notifIt.js')}}"></script>
-    <script src="{{URL::asset('/plugins/notify/js/notifit-custom.js')}}"></script>
+<!--Internal  Notify js -->
+<script src="{{URL::asset('dashboard/plugins/notify/js/notifIt.js')}}"></script>
+<script src="{{URL::asset('/plugins/notify/js/notifit-custom.js')}}"></script>
 
 @endsection
-
