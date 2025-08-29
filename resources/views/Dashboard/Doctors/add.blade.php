1c8389c73fe3ef4ca3529dfe43b1a3d309d41d7f
rashad al shukh <rashad000089@gmail.com>
2025-08-29 21:23:37 +0300

    test

--- resources/views/Dashboard/Doctors/add.blade.php
+++ resources/views/Dashboard/Doctors/add.blade.php
@@ -101,7 +101,7 @@
 
                             </div>
 
-                            <div class="row row-xs align-items-center mg-b-20">
+                            <!-- <div class="row row-xs align-items-center mg-b-20">
                                 <div class="col-md-1">
                                     <label for="exampleInputEmail1">
                                         {{trans('doctors.appointments')}}</label>
@@ -117,7 +117,7 @@
 
                                 </div>
 
-                            </div>
+                            </div> -->
 
                             <div class="row row-xs align-items-center mg-b-20">
                                 <div class="col-md-1">
