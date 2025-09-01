74ba585eb783181fca2b96944014b61f73c48deb
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-02 01:18:30 +0300

    localize ray employee views

---
+++ resources/lang/en/RayEmployee.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'Rays' => 'Rays',
+    'EmployeeList' => 'Employee List',
+    'AddEmployee' => 'Add Employee',
+    'EditEmployee' => 'Edit Employee',
+    'DeleteEmployee' => 'Delete Employee',
+    'Name' => 'Name',
+    'Email' => 'Email',
+    'Password' => 'Password',
+    'CreatedAt' => 'Created At',
+    'Actions' => 'Actions',
+    'Close' => 'Close',
+    'Submit' => 'Submit',
+];
