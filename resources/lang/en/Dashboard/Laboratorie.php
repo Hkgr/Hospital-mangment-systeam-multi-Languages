a8a5f999baf79b9921a3e5eeaa1ae14a4a208cfa
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-02 01:07:59 +0300

    Add laboratory translation files

---
+++ resources/lang/en/Dashboard/Laboratorie.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Laboratory' => 'Laboratory',
+    'Invoices' => 'Invoices',
+    'ServiceName' => 'Service Name',
+    'DoctorName' => 'Doctor Name',
+    'LabEmployeeName' => 'Lab Employee Name',
+    'CaseStatus' => 'Case Status',
+    'ViewAnalysis' => 'View Analysis',
+    'LabNotes' => 'Lab Employee Notes',
+    'Completed' => 'Completed',
+    'NotCompleted' => 'Not Completed',
+];
+
