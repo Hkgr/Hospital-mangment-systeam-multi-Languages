0983e996c26df7a787c87060dcfa79397e6e8f51
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-01 23:34:14 +0300

    Add appointment creation notification channel

--- routes/channels.php
+++ routes/channels.php
@@ -21,6 +21,7 @@ Broadcast::channel('create-single-service.admin', fn($user) => auth('admin')->ch
 Broadcast::channel('create-group-service.admin', fn($user) => auth('admin')->check(), ['guards' => ['admin']]);
 Broadcast::channel('create-patient.admin', fn($user) => auth('admin')->check(), ['guards' => ['admin']]);
 Broadcast::channel('create-payment.admin', fn($user) => auth('admin')->check(), ['guards' => ['admin']]);
+Broadcast::channel('create-appointment.admin', fn($user) => auth('admin')->check(), ['guards' => ['admin']]);
 Broadcast::channel(
     'create-invoice.{doctor_id}',
     function ($user, $doctor_id) {
