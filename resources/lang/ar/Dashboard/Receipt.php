27836cbd7483dd4d72c839a268ca92a646a58fdf
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-02 00:39:47 +0300

    Add translations for receipt print view

---
+++ resources/lang/ar/Dashboard/Receipt.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Title' => 'معاينة الطباعة',
+    'ReceiptVoucher' => 'سند قبض',
+    'BreadcrumbPrint' => 'طباعه سند',
+    'ProgramName' => 'برنامج ادراه المستشفي',
+    'UniversityName' => 'جامعة حلب الحرة',
+    'ReceiptInfo' => 'معلومات السند',
+    'IssueDate' => 'تاريخ الاصدار',
+    'PatientName' => 'اسم المريض',
+    'Notes' => 'ملاحظات',
+    'Amount' => 'المبلغ',
+    'PrintButton' => 'طباعه',
+];
+
