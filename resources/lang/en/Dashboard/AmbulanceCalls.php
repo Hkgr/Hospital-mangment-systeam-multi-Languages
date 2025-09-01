1a0a97ab5a0a974f7b485798475344ad1cfc6c71
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-02 00:09:02 +0300

    add ambulance calls language files

---
+++ resources/lang/en/Dashboard/AmbulanceCalls.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'Ambulance' => 'Ambulance',
+    'Calls' => 'Ambulance Calls',
+    'Phone' => 'Phone Number',
+    'CallTime' => 'Call Time',
+    'AmbulanceCar' => 'Ambulance Car',
+    'Address' => 'Call Address',
+    'Status' => 'Status',
+    'Operations' => 'Operations',
+    'FirstAid' => 'First Aid',
+    'TransferToHospital' => 'Transfer to Hospital',
+    'TransferToAnotherHospital' => 'Transfer to Another Hospital',
+    'Unknown' => 'Unknown',
+    'Delete' => 'Delete',
+];
+
