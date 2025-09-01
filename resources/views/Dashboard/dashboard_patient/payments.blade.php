a5a3e8e0599353cb06ada3a372ad3de5a93c1828
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-02 00:51:52 +0300

    feat: add patient translation files

--- resources/views/Dashboard/dashboard_patient/payments.blade.php
+++ resources/views/Dashboard/dashboard_patient/payments.blade.php
@@ -13,7 +13,7 @@
 				<div class="breadcrumb-header justify-content-between">
 					<div class="my-auto">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">عمليات المريض</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ المدفوعات</span>
+                                                        <h4 class="content-title mb-0 my-auto">{{ trans('Dashboard/Patient.PatientOperations') }}</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ {{ trans('Dashboard/Patient.Payments') }}</span>
 						</div>
 					</div>
 				</div>
@@ -34,9 +34,9 @@
                                             <thead>
                                             <tr>
                                                 <th>#</th>
-                                                <th>تاريخ الدفع</th>
-                                                <th >المبلغ</th>
-                                                <th>البيان</th>
+                                                <th>{{ trans('Dashboard/Patient.PaymentDate') }}</th>
+                                                <th>{{ trans('Dashboard/Patient.Amount') }}</th>
+                                                <th>{{ trans('Dashboard/Patient.Description') }}</th>
                                             </tr>
                                             </thead>
                                             <tbody>
