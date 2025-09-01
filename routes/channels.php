78da09fed29e97c1172c9a5f5d55176be961533a
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-02 00:17:33 +0300

    feat: broadcast ray events

--- routes/channels.php
+++ routes/channels.php
@@ -22,6 +22,7 @@ Broadcast::channel('create-group-service.admin', fn($user) => auth('admin')->che
 Broadcast::channel('create-patient.admin', fn($user) => auth('admin')->check(), ['guards' => ['admin']]);
 Broadcast::channel('create-payment.admin', fn($user) => auth('admin')->check(), ['guards' => ['admin']]);
 Broadcast::channel('create-appointment.admin', fn($user) => auth('admin')->check(), ['guards' => ['admin']]);
+Broadcast::channel('create-ray.ray_employee', fn($user) => auth('ray_employee')->check(), ['guards' => ['ray_employee']]);
 Broadcast::channel(
     'create-invoice.{doctor_id}',
     function ($user, $doctor_id) {
@@ -81,6 +82,22 @@ Broadcast::channel(
 
 Broadcast::channel(
     'create-diagnosis.doctor.{doctor_id}',
+    function ($user, $doctor_id) {
+        return $user->id == $doctor_id;
+    },
+    ['guards' => ['web', 'admin', 'patient', 'doctor', 'ray_employee', 'laboratorie_employee', 'api']]
+);
+
+Broadcast::channel(
+    'create-ray.patient.{patient_id}',
+    function ($user, $patient_id) {
+        return $user->id == $patient_id;
+    },
+    ['guards' => ['web', 'admin', 'patient', 'doctor', 'ray_employee', 'laboratorie_employee', 'api']]
+);
+
+Broadcast::channel(
+    'create-ray.doctor.{doctor_id}',
     function ($user, $doctor_id) {
         return $user->id == $doctor_id;
     },
