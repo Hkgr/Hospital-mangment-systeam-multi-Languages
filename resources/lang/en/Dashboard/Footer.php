c348b9e0e3e2fe4fd99010f332ad10a73059b7bb
Rashad Al-Shiekh <rashad000089@gmail.com>
2025-09-01 23:12:37 +0300

    Add translation support for dashboard footer

---
+++ resources/lang/en/Dashboard/Footer.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'Copyright' => 'Copyright © 2025 <a href="#">HMS</a>. Designed by <a href="https://127.0.0.1/">Rashad Al-hiekh</a> All rights reserved.',
+];
--- resources/views/Dashboard/layouts/footer.blade.php
+++ resources/views/Dashboard/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
-	<div class="main-footer ht-40">
-		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>Copyright © 2025 <a href="#">HMS</a>. Designed by <a href="https://127.0.0.1/">Rashad Al-hiekh</a> All rights reserved.</span>
-		</div>
-	</div>
+        <div class="main-footer ht-40">
+                <div class="container-fluid pd-t-0-f ht-100p">
+                        <span>{!! trans('Dashboard/Footer.Copyright') !!}</span>
+                </div>
+        </div>
 <!-- Footer closed -->
